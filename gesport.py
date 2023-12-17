d07880be6be6d20bf0983ac47ac8500d65c8ac96
Alexandre Dumont <alexandre.dumont.7@ulaval.ca>
2023-12-17 14:32:54 -0500

    def main and anal completed

--- gesport.py
+++ gesport.py
@@ -6,69 +6,72 @@ from exceptions import *
 
 
 def analyser_commande():
-    parser = argparse.ArgumentParser(description="Gestionnaire de portefeuille d'actions.")
+    parser = argparse.ArgumentParser(description="Gestionnaire de portefeuille d'actions")
     
-    parser.add_argument("action", help="Nom de l'action à gérer")
-    parser.add_argument("-d", "--date", default=date.today(), type=lambda d: date.fromisoformat(d),
-                        help="Date effective (par défaut, date du jour)")
-    parser.add_argument("-q", "--quantité", default=1, type=int,
-                        help="Quantité désirée (par défaut: 1)")
-    parser.add_argument("-t", "--titres", nargs='*', default=None,
-                        help="Le ou les titres à considérer (par défaut, tous les titres du portefeuille)")
-    parser.add_argument("-r", "--rendement", default=0.0, type=float,
-                        help="Rendement annuel global (par défaut, 0)")
-    parser.add_argument("-v", "--volatilité", default=0.0, type=float,
-                        help="Indice de volatilité global sur le rendement annuel (par défaut, 0)")
-    parser.add_argument("-g", "--graphique", action='store_true',
-                        help="Affichage graphique (par défaut, pas d'affichage graphique)")
-    parser.add_argument("-p", "--portefeuille", default="folio", type=str,
-                        help="Nom de portefeuille (par défaut, utiliser 'folio')")
+    subparsers = parser.add_subparsers(dest='action', required=True)
 
-    args = parser.parse_args()
+    # Sous-commande 'déposer'
+    parser_deposer = subparsers.add_parser('déposer', help="Déposer des liquidités dans le portefeuille")
+    parser_deposer.add_argument("--date", default=date.today(), type=lambda d: date.fromisoformat(d), help="Date de dépôt (format: AAAA-MM-JJ)")
+    parser_deposer.add_argument("--quantité", type=float, required=True, help="Montant à déposer")
 
-    # Correction des valeurs par défaut si nécessaire
-    if args.début is None and args.fin:
-        args.début = args.fin
+    # Sous-commande 'acheter'
+    parser_acheter = subparsers.add_parser('acheter', help="Acheter des actions")
+    parser_acheter.add_argument("--date", default=date.today(), type=lambda d: date.fromisoformat(d), help="Date d'achat (format: AAAA-MM-JJ)")
+    parser_acheter.add_argument("--titres", nargs='+', required=True, help="Symbole(s) des titres à acheter")
+    parser_acheter.add_argument("--quantité", type=int, required=True, help="Quantité à acheter")
 
-    if args.fin is None and args.début:
-        args.fin = args.début
+    # Sous-commande 'vendre'
+    parser_vendre = subparsers.add_parser('vendre', help="Vendre des actions")
+    parser_vendre.add_argument("--date", default=date.today(), type=lambda d: date.fromisoformat(d), help="Date de vente (format: AAAA-MM-JJ)")
+    parser_vendre.add_argument("--titres", nargs='+', required=True, help="Symbole(s) des titres à vendre")
+    parser_vendre.add_argument("--quantité", type=int, required=True, help="Quantité à vendre")
 
-    if args.début == args.fin:
-        args.fin = args.début
-    
-    return args
+    # Sous-commande 'lister'
+    parser_lister = subparsers.add_parser('lister', help="Lister les actions du portefeuille")
+    parser_lister.add_argument("--date", default=date.today(), type=lambda d: date.fromisoformat(d), help="Date pour la liste (format: AAAA-MM-JJ)")
+
+    # Sous-commande 'projeter'
+    parser_projeter = subparsers.add_parser('projeter', help="Projeter la valeur future du portefeuille")
+    parser_projeter.add_argument("--date", required=True, type=lambda d: date.fromisoformat(d), help="Date future de projection (format: AAAA-MM-JJ)")
+    parser_projeter.add_argument("--rendement", type=float, required=True, help="Rendement annuel attendu (en %)")
+    parser_projeter.add_argument("--volatilité", type=float, default=0.0, help="Indice de volatilité sur le rendement annuel (par défaut, 0)")
+
+
 
 def main():
     args = analyser_commande()
-
-    # Initialisation des objets Bourse et Portefeuille
     bourse = Bourse()
-    portefeuille = Portefeuille(bourse, args.portefeuille)
+    portefeuille = Portefeuille(bourse)
 
-    # Traitement des différentes actions basées sur les arguments de la ligne de commande
-    if args.action == "acheter":
-        # Vérification et achat des titres spécifiés
+    if args.action == "déposer":
+        # Logique pour déposer
+        montant = portefeuille.deposer(args.quantité, args.date)
+        print(f"solde = {montant:.2f}")
+
+    elif args.action == "acheter":
+        # Logique pour acheter
         for titre in args.titres:
             portefeuille.acheter(titre, args.quantité, args.date)
+        solde = portefeuille.solde(args.date)
+        print(f"solde = {solde:.2f}")
+
     elif args.action == "vendre":
-        # Vérification et vente des titres spécifiés
+        # Logique pour vendre
         for titre in args.titres:
             portefeuille.vendre(titre, args.quantité, args.date)
-    
+        solde = portefeuille.solde(args.date)
+        print(f"solde = {solde:.2f}")
+    elif args.action == "lister":
+        # Logique pour lister
+        actions = portefeuille.lister(args.date)
+        for symbole, (quantité, prix, montant) in actions.items():
+            print(f"{symbole} = {quantité} x {prix:.2f} = {montant:.2f}")
 
-    # Gérer l'affichage graphique si demandé
-    if args.graphique:
-        # Code pour générer et afficher le graphique
-        pass
-
-    # Traiter les options de rendement et de volatilité
-    if args.rendement or args.volatilité:
-        # Code pour gérer le rendement et la volatilité
-        pass
-
-    # Traiter les autres options/arguments selon les besoins
+    elif args.action == "projeter":
+        # Logique pour projeter
+        valeur_projete = portefeuille.projeter(args.date, args.rendement, args.volatilité)
+        print(f"valeur projetée = {valeur_projete:.2f}")
 
 if __name__ == "__main__":
     main()
-
-
